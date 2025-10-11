36c4815b5e5428e33229e988c9dbc62d7a6c18cf
Taras Shpek <taras.shpek@gmail.com>
2025-10-11 11:02:49 +0300

    Add autoconfiguration

---
+++ src/main/java/software/plusminus/spring/config/SpringAutoconfig.java
@@ -0,0 +1,9 @@
+package software.plusminus.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("software.plusminus.spring")
+public class SpringAutoconfig {
+}
